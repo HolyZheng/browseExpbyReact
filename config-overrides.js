e2134f10315d6474a535272d2f14647d288e0e26
holyzheng <holzpps@163.com>
2018-10-29 21:16:19 +0800

    first commit

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,31 @@
 /* config-overrides.js */
 
 const rewireTypescript = require('react-app-rewire-typescript');
+const workboxPlugin = require('workbox-webpack-plugin')
+const path = require('path')
 
-module.exports = function override(config, env) {
-    // ...
+module.exports = {
+  webpack: function (config, env) {
     config = rewireTypescript(config, env);
-    // ...
-    return config;
+    if (env === 'production') {
+      const workboxConfigProd = {
+        swSrc: path.join(__dirname, 'public', 'cus-service-worker.js'),
+        swDest: 'cus-service-worker.js',
+        importWorkboxFrom: 'disabled'
+      }
+      config = removePreWorkboxWebpackPluginConfig(config)
+      config.plugins.push(new workboxPlugin.InjectManifest(workboxConfigProd))
+    }
+    return config
+  }
+}
+
+function removePreWorkboxWebpackPluginConfig (config) {
+  const preWorkboxPluginIndex = config.plugins.findIndex((element) => {
+    return Object.getPrototypeOf(element).constructor.name === 'GenerateSW'
+  })
+  if (preWorkboxPluginIndex !== -1) {
+    config.plugins.splice(preWorkboxPluginIndex, 1)
+  }
+  return config
 }
